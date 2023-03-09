8a55576ea94746130687ee99945e67413331176b
Miwako <you@example.com>
2023-03-09 13:11:57 +0000

    w

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -11,15 +11,19 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_image = PostImage.all
 
   end
 
 
   def show
+     @post_image = PostImage.find(params[:id])
   end
 
   def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to '/PostImage'
   end
 
 
